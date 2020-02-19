c4c252261d0d6bbfa26d3e01ce280a91f573cb90
Sahil Bhanderi <sahil.bhanderi@gmail.com>
2020-02-19 16:29:41 -0500

    Adding missing files

---
+++ DemoApp/About.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="DemoApp.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.</p>
+    <p>
+        <asp:TextBox ID="txtData" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSendInfo" runat="server" OnClick="btnSendInfo_Click" Text="Send Info" />
+    </p>
+</asp:Content>
---
+++ DemoApp/About.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DemoApp
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Title = "From code behind";
+                Trace.Write("From code", "No postback");
+            }
+            else
+            {
+                Title = "Postback data: " + txtData.Text;
+                Trace.Write("From code", "Postback: " + txtData.Text);
+            }
+        }
+
+        protected void btnSendInfo_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtData.Text))
+            {
+                throw new ApplicationException("Something unthinkable happened!");
+            }
+        }
+
+        protected void Page_Error(object sender, EventArgs e)
+        {
+            // Log the information somewhere
+            var ex = Server.GetLastError();
+            Server.ClearError();
+            Response.Write(ex.Message);
+        }
+    }
+}
