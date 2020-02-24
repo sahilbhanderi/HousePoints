4e95a0a857fabaf0e1054e8cef298193f0c13a1c
ShiYuanChen7 <sbc5438@psu.edu>
2020-02-24 01:48:24 -0500

    adding all files I created for new version

---
+++ Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousePointsApp.Models
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string UID { get; set; }
+        public int HousePoint { get; set; }
+
+    }
+}
---
+++ Pages/LoginPage.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HousePointsApp.Models;
+
+namespace HousePointsApp
+{
+    public class AddUserModel : PageModel
+    {
+        [BindProperty]
+        public UserModel UserAccount { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        { 
+            if (ModelState.IsValid == false)
+            { return Page(); }
+            return RedirectToPage("./Response/_LoginSuccess");
+        }
+    }
+}
