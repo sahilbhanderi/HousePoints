1e2f2680a7453d05b4c417f9244a349d09b70a57
Sahil <skb5400@psu.edu>
2020-03-04 12:25:06 -0500

    Added model files

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousePointsApp.Models
+{
+    public class Student
+    {
+        public int student_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public int total_points { get; set; }
+        public string studentid { get; set; }
+    }
+}
