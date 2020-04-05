34d11263ba79fc8e26a494af5e2f5ef44f84b248
Kelsie McElroy <kelsie.n.mcelroy@gmail.com>
2020-04-05 14:23:20 -0400

    Fix issue in Attendance Data Service

--- DataServices/AttendanceDataService.cs
+++ DataServices/AttendanceDataService.cs
@@ -1,179 +1,144 @@
+using System;
+using System.Data.SqlClient;
 using HousePointsApp.Interfaces;
 using HousePointsApp.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace HousePointsApp.DataServices
+
 {
-    public class StudentDataService : IStudentDataService
+    public class AttendanceDataService : IAttendanceDataService
     {
         private String CONNECTION_STRING = @"Data Source=localhost;Initial Catalog=The_Learning_Factory_Points_System;" +
             "User ID=sa;Password=YourPasswordHere";
 
-        public String GetFirstName(String studentId)
-        {
-            // Query LionPath view for student's first name
-
-             SqlConnection cnn = new SqlConnection(CONNECTION_STRING);
-             cnn.Open();
-
-             String getFirstNameSql = "SELECT first_name FROM vw_LF_Students WHERE student_id = " + studentId;
-             SqlCommand getFirstNameCommand = new SqlCommand(getFirstNameSql, cnn);
-
-             SqlDataReader getFirstNameReader = getFirstNameCommand.ExecuteReader();
-             String first_name = "";
-
-             while (getFirstNameReader.Read())
-             {
-                 first_name = getFirstNameReader.GetValue(0).ToString();
-             }
-
-             return first_name;
-        }
-
-        public String GetLastName(String studentId)
-        {
-            // Query LionPath view for student's last name
-
-             SqlConnection cnn = new SqlConnection(CONNECTION_STRING);
-             cnn.Open();
-
-             String getLastNameSql = "SELECT last_name FROM vw_LF_Students WHERE student_id = " + studentId;
-             SqlCommand getLastNameCommand = new SqlCommand(getLastNameSql, cnn);
-
-             SqlDataReader getLastNameReader = getLastNameCommand.ExecuteReader();
-             String last_name = "";
-
-             while (getLastNameReader.Read())
-             {
-                 last_name = getLastNameReader.GetValue(0).ToString();
-             }
-
-             return last_name;
-        }
-
         public String GetCampusId(String studentId)
         {
-            // Query LionPath view for student's campus id
-
-             SqlConnection cnn = new SqlConnection(CONNECTION_STRING);
-             cnn.Open();
-
-             String getCampusIdSql = "SELECT campus_id FROM vw_LF_Students WHERE student_id = " + studentId;
-             SqlCommand getCampusIdCommand = new SqlCommand(getCampusIdSql, cnn);
-
-             SqlDataReader getCampusIdReader = getCampusIdCommand.ExecuteReader();
-             String campus_id = "";
-
-             while (getCampusIdReader.Read())
-             {
-                 campus_id = getCampusIdReader.GetValue(0).ToString();
-             }
-
-             return campus_id;
-        }
-
-        public Student GetStudent(String studentId)
-        {
-            // Create and execute SQL query to retrieve a student record
+            // Lionpath View Query
 
             SqlConnection cnn = new SqlConnection(CONNECTION_STRING);
             cnn.Open();
-            String getStudentSql = "SELECT * FROM STUDENT WHERE student_id = " + studentId + ";";
 
-            Student student = new Student();
+            String getCampusIdSql = "SELECT campus_id FROM vw_LF_Students WHERE student_id =" + studentId;
+            SqlCommand getCampusIdCommand = new SqlCommand(getCampusIdSql, cnn);
 
-            SqlCommand getStudentCommand = new SqlCommand(getStudentSql, cnn);
-            SqlDataReader getStudentReader = getStudentCommand.ExecuteReader();
+            SqlDataReader getCampusIdReader = getCampusIdCommand.ExecuteReader();
+            String campus_id = "";
 
-            try
+            while (getCampusIdReader.Read())
             {
-                while (getStudentReader.Read())
-                {
-                    student.student_id = getStudentReader.GetValue(0).ToString();
-                    student.campus_id = getStudentReader.GetValue(1).ToString();
-                    student.first_name = getStudentReader.GetValue(2).ToString();
-                    student.last_name = getStudentReader.GetValue(3).ToString();
-                    student.total_points = Convert.ToInt32(getStudentReader.GetValue(4));
-                }
-                cnn.Close();
-
-                return student;
-            } catch
-            {
-                cnn.Close();
-
-                return null;
+                campus_id = getCampusIdReader.GetValue(0).ToString();
             }
+
+            return campus_id;
         }
 
-        public Boolean CreateStudent(String studentId)
+        public Boolean CreateAttendance(String studentId)
         {
-            // Get first name, last name, and campus id for corresponding
-            // student id
-
-            String first_name = GetFirstName(studentId);
-            String last_name = GetLastName(studentId);
+            // Get the campus id (abc123) for given student id
             String campus_id = GetCampusId(studentId);
 
-            // Create and execute a SQL query to create a new student record
+            // Create and execute a SQL query that creates a new attendance
+            // record for a student
 
             SqlConnection cnn = new SqlConnection(CONNECTION_STRING);
             cnn.Open();
 
-            String insertStudentSql = "INSERT INTO STUDENT " +
-                "(student_id, campus_id, first_name, last_name, total_points) " +
-                "VALUES (" + studentId + ", '" + campus_id + "', '" +
-                first_name + "', '" + last_name + "'," + 0 + ");";
+            DateTime myDateTime = DateTime.Now;
+            string sqlFormattedDate = myDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
 
-            SqlCommand insertStudentCommand = new SqlCommand(insertStudentSql, cnn);
+            String createAttendanceSql = "INSERT INTO attendance (campus_id, check_in) VALUES('" + campus_id + "', '" + sqlFormattedDate + "');";
+
+            SqlCommand createAttendanceCommand = new SqlCommand(createAttendanceSql, cnn);
 
             try
             {
-                insertStudentCommand.ExecuteNonQuery();
+                createAttendanceCommand.ExecuteNonQuery();
                 return true;
-            } catch
+            }
+            catch
             {
                 return false;
             }
         }
 
-        public Boolean DeleteStudent(String userId)
+        public Attendance GetLatestAttendance(String studentId)
         {
-            // Create and execute a SQL query to delete a student's record based
-            // on the campusId (abc123) provided by administrator
+            // Get campus id from Lionpath view
+            String campusId = GetCampusId(studentId);
+
+            // Create and execute SQL query to find latest attendance log for
+            // a student
 
             SqlConnection cnn = new SqlConnection(CONNECTION_STRING);
             cnn.Open();
 
-            String deleteAttendanceSql = "DELETE FROM attendance WHERE " +
-                "'campus_id'= " + userId + ";";
+            Attendance attendance = new Attendance();
 
-            String deleteStudentSql = "DELETE FROM student WHERE " +
-                "'campus_id'= " + userId + ";";
+            String getLatestAttendance = "SELECT * FROM attendance WHERE session_id = (SELECT MAX(session_id) FROM attendance " +
+                "WHERE campus_id = '" + campusId + "');";
 
-            SqlCommand deleteAttendanceCommand = new SqlCommand(deleteAttendanceSql, cnn);
-            SqlCommand deleteStudentCommand = new SqlCommand(deleteStudentSql, cnn);
+            SqlCommand getLatestAttendanceCommand = new SqlCommand(getLatestAttendance, cnn);
+
+            SqlDataReader getLatestAttendanceReader = getLatestAttendanceCommand.ExecuteReader();
+
+            while (getLatestAttendanceReader.Read())
+            {
+                attendance.session_id = Convert.ToInt32(getLatestAttendanceReader.GetValue(0));
+                attendance.campus_id = getLatestAttendanceReader.GetValue(1).ToString();
+                attendance.check_in = Convert.ToDateTime(getLatestAttendanceReader.GetValue(2));
+                if (getLatestAttendanceReader.GetValue(3) is DBNull)
+                {
+                    attendance.check_out = null;
+                }
+                else
+                {
+                    attendance.check_out = Convert.ToDateTime(getLatestAttendanceReader.GetValue(3));
+
+                }
+
+                if (getLatestAttendanceReader.GetValue(4) is DBNull)
+                {
+                    attendance.session_points = null;
+                }
+                else
+                {
+                    attendance.session_points = Convert.ToInt32(getLatestAttendanceReader.GetValue(4));
+                }
+
+            }
+
+            return attendance;
+        }
+
+        public Boolean UpdateSession(String sessionId)
+        {
+            // Create and execute SQL query to update the check out time and points
+            // for a student's attendance record
+
+            SqlConnection cnn = new SqlConnection(CONNECTION_STRING);
+            cnn.Open();
+
+            DateTime myDateTime = DateTime.Now;
+            string sqlFormattedDate = myDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+            String UpdateSession = "UPDATE attendance SET check_out = '" +
+                sqlFormattedDate + "' WHERE session_id = " + sessionId + ";";
+
+            String UpdatePoints = "UPDATE attendance SET session_points = " +
+                1 + " WHERE session_id = " + sessionId + ";";
+
+            SqlCommand setUpdatedSession = new SqlCommand(UpdateSession, cnn);
+            SqlCommand setUpdatedPoints = new SqlCommand(UpdatePoints, cnn);
 
             try
             {
-                deleteAttendanceCommand.ExecuteNonQuery();
-                deleteStudentCommand.ExecuteNonQuery();
-
+                setUpdatedSession.ExecuteNonQuery();
+                setUpdatedPoints.ExecuteNonQuery();
                 return true;
-            } catch
+            }
+            catch
             {
                 return false;
             }
         }
-
     }
 }
